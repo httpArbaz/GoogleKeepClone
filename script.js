204d30d5fddf39c9976fc15fdbe59f3f9c582631
httpArbaz <128707115+httpArbaz@users.noreply.github.com>
2023-04-10 22:41:40 +0500

    Html Js Css

---
+++ script.js
@@ -0,0 +1,51 @@
+const addButton = document.querySelector("#add");
+
+const addNewNote = (text = '') => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  const htmlData = `<div class="operation">
+    <button class="edit"><i class="fas fa-edit"></i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main ${text ? "" : "hidden"} "></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
+
+    note.insertAdjacentHTML('afterbegin', htmlData);
+
+     // getting refrence
+
+     const editButton = note.querySelector('.edit')
+     const delButton = note.querySelector('.delete') 
+     const mainDiv = note.querySelector('.main')
+     const textarea = note.querySelector('textarea')
+
+    // delete the node
+
+    delButton.addEventListener('click', ()=>{
+        note.remove();
+    })
+
+    // toogle using editbutton
+    
+    textarea.value = text;
+    mainDiv.innerHTML = text;
+
+    editButton.addEventListener('click', ()=>{
+        mainDiv.classList.toggle('hidden')        
+        textarea.classList.toggle('hidden')        
+    })
+
+    textarea.addEventListener('change', (event)=>{
+        const value = event.target.value;
+        mainDiv.innerHTML = value;
+ 
+        
+    })
+
+    document.body.appendChild(note);
+ 
+};
+
+addButton.addEventListener("click", () => addNewNote());
